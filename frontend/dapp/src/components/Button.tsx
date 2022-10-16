6b11b009cb2a1639b5d4e71092f9ff730257e53a
Andrew <63206688+AndrewGonnaCode@users.noreply.github.com>
2022-10-16 17:34:04 +0600

    components-init

---
+++ frontend/dapp/src/components/Button.tsx
@@ -0,0 +1,21 @@
+import React, { ReactNode } from 'react'
+import { ButtonType } from '../models'
+
+interface IButtonProps {
+    children:ReactNode,
+    type:ButtonType,
+    onClick:()=>Promise<void>
+}
+
+const Button = ({children, type, onClick}:IButtonProps) => {
+  return (
+    <button onClick={onClick} className={`ml-3 inline-flex rounded-md shadow
+     items-center justify-center rounded-md border border-transparent 
+     bg-white px-5 py-3 text-base font-medium 
+     text-indigo-600 hover:bg-indigo-50`}>
+        {children}
+    </button>
+  )
+}
+
+export default Button
